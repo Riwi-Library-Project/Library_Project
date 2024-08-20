b605eeb4c50b4bd9791a7377bb6c155e91b9a1d0
AarFeE <44449694+AarFeE@users.noreply.github.com>
2024-08-20 02:19:33 -0500

    Add files via upload

---
+++ src/main/java/riwi/bezos/virtual_book/VirtualBookApplication.java
@@ -0,0 +1,13 @@
+package riwi.bezos.virtual_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBookApplication.class, args);
+	}
+
+}
